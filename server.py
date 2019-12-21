d91ee8253d58ca871cdb4a3ffa1e753fe0b735c2
evanwilt <ev.wilt@gmail.com>
2019-12-21 15:49:07 -0500

    Moving to use json over paths

--- server.py
+++ server.py
@@ -2,20 +2,38 @@
 
 import asyncio
 import websockets
+import json
 
-async def connect(websocket):
-    payload = await websocket.recv()
-    print(payload)
-    await websocket.send(payload)
+rooms = {}
 
-routes = {
-    "/connect": connect
+
+async def create_room(websocket, payload):
+    if payload["room_code"] in rooms.keys():
+        await send_error(websocket, "Room already exists")
+    else:
+        rooms[payload["room_code"]] = []
+
+actions = {
+    "create_room": create_room
 }
 
-async def router(websocket, path):
-    await routes[path](websocket)
+async def send_error(websocket, error_msg):
+    json_msg = json.dumps({
+        "error": error_msg
+    })
+    await websocket.send(json_msg)
 
-start_server = websockets.serve(router, "localhost", 8765)
+async def server_loop(websocket, path):
+    while True:
+        payload = json.loads(await websocket.recv())
+        if "action" not in payload.keys():
+            await send_error(websocket, "No action provided")
+        else:
+            if payload["action"] in actions.keys():
+                await actions[payload["action"]](websocket, payload)
+
+start_server = websockets.serve(server_loop, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
+print("Server started")
 asyncio.get_event_loop().run_forever()
